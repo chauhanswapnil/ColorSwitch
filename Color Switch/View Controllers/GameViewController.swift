d2884d63fcb8631651ec4109681fe607db29cccb
Swapnil Chauhan <swapnilchauhan@ymail.com>
2018-07-09 20:31:58 +0530

    Game Complete

--- Color Switch/View Controllers/GameViewController.swift
+++ Color Switch/View Controllers/GameViewController.swift
@@ -16,7 +16,7 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
     
-            let scene = GameScene(size: view.bounds.size)
+            let scene = MenuScene(size: view.bounds.size)
             
             scene.scaleMode = .aspectFill
             view.presentScene(scene)
